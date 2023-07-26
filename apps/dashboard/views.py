2fee2ec8b450e64865cbe2d14b549696f02e1d2c
Michael Mg <michael.mg.09a@gmail.com>
2023-07-26 22:19:03 +0200

    update

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -10,10 +10,12 @@ def dashboard(request):
 
     objects_investisments = Investisment()
     objects_buy_asset = BuyAsset()
+    usdt = BuyAsset.objects.get(name='usdt')
 
     return render(request,
                   'dashboard/dashboard.html',
                   {'total_invest': objects_investisments.total_invest(),
                    'total_invest_actual': objects_buy_asset.current_invest(),
                    'profit_actual': objects_investisments.profit(),
+                   'available_reinvestment': usdt.amount,
                    'text': 'No data'})
